9715abe283f1fae63a67004da0f7e4e5fc573feb
corey-rosamond <97810442+corey-rosamond@users.noreply.github.com>
2022-02-18 07:50:02 -0800

    Adding very basic nav bar

--- src/component/NavigationComponent.js
+++ src/component/NavigationComponent.js
@@ -1,9 +1,71 @@
 import React from "react";
+import styled from "styled-components";
 import {
-  Navbar,
-  Container,
-  Nav
-} from "react-bootstrap";
+  Badge
+} from "@mui/material"
+
+import {
+  Search,
+  ShoppingCartOutlined
+} from "@mui/icons-material";
+
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 10px;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const LanguageSelector = styled.span`
+  font-size: 18px;
+  cursor: pointer;
+  font-weight: bold;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgrey;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+
+`;
+
+const SearchInput = styled.input`
+  border: none;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0 10px 0 0;
+`;
+
+const MenuItem = styled.div`
+  font-size: 18px;
+  cursor: pointer;
+  margin-left: 25px;
+  font-weight: bold;
+`;
+
 
 /**
  * NavigationComponent
@@ -39,21 +101,29 @@ class NavigationComponent extends React.Component
   render()
   {
     return (
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href={this.state.brandDestination}>{this.state.brandText}</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href={this.state.homeDestination}>{this.state.homeText}</Nav.Link>
-              {this.state.navigationLinks.map((item)=>{
-                let [text, destination] = item;
-                return <Nav.Link href={destination} key={destination}>{text}</Nav.Link>
-              })}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Wrapper>
+        <Left>
+          <LanguageSelector>
+            EN/Chinese
+          </LanguageSelector>
+          <SearchContainer>
+            <SearchInput />
+            <Search style={{color: "grey", fontSize:22}} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>The Mystery Market</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Login</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined/>
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
     )
   }
 }
