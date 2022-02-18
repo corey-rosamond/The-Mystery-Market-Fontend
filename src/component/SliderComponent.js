fcbf7a9d9847e130f318b4343d3d76c86b0693dd
corey-rosamond <97810442+corey-rosamond@users.noreply.github.com>
2022-02-18 14:40:29 -0800

    Added data directory to hold temporary data until I integrate it with the back end. Also added a slider component.

--- src/component/SliderComponent.js
+++ src/component/SliderComponent.js
@@ -1,13 +1,17 @@
 import React from "react";
 import styled from "styled-components";
-import {ArrowLeftOutlined, ArrowRightOutlined} from "@mui/icons-material";
+import {
+  ArrowLeftOutlined,
+  ArrowRightOutlined
+} from "@mui/icons-material";
+import {Slides} from "../data/Slides";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: lightblue;
   position: relative;
+  overflow:hidden;
 `;
 
 const Arrow = styled.div`
@@ -26,21 +30,122 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.7;
+  z-index: 100;
 `;
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${props=>props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${props=>props.bg};
+`;
+
+const ImageContainer = styled.div`
+  height: 100%;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InformationContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+
+const Description = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+`;
+
+/**
+ * SliderComponent
+ */
 class SliderComponent extends React.Component
 {
+  /**
+   * constructor
+   * @param props
+   */
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      slides: Slides,
+      slideIndex: 0,
+    };
+  }
 
+  /**
+   * handleClick
+   * @param direction
+   */
+  handleClick(direction)
+  {
+    if(direction === "left")
+    {
+      this.setState({
+        slideIndex: (this.state.slideIndex > 0 ? this.state.slideIndex-1:this.state.slides.length-1)
+      });
+    } else
+    {
+      this.setState({
+        slideIndex: (this.state.slideIndex < (this.state.slides.length-1) ? this.state.slideIndex+1: 0)
+      });
+    }
+    console.log(this.state.slides.length);
+  }
+
+  /**
+   * render
+   * @returns {JSX.Element}
+   */
   render()
   {
 
     return (
       <Container>
-        <Arrow direction="left">
+        <Arrow direction="left" onClick={() => this.handleClick("left")}>
           <ArrowLeftOutlined/>
         </Arrow>
-
-        <Arrow direction="right">
+        <Wrapper slideIndex={this.state.slideIndex}>
+          {this.state.slides.map(slide => {
+            return (
+              <Slide key={slide._id} bg={slide.backgroundColor}>
+                <ImageContainer>
+                  <Image src={slide.image} />
+                </ImageContainer>
+                <InformationContainer>
+                  <Title>{slide.title}</Title>
+                  <Description>{slide.description}</Description>
+                  <Button>{slide.button}</Button>
+                </InformationContainer>
+              </Slide>
+            );
+          })}
+        </Wrapper>
+        <Arrow direction="right" onClick={() => this.handleClick("right")}>
           <ArrowRightOutlined/>
         </Arrow>
       </Container>
---
+++ src/data/Slides.js
@@ -0,0 +1,50 @@
+export const Slides = [
+  {
+    _id: 1,
+    image: "products/LED Sanitizing Wand 1.jpg",
+    title: "LED UVC SANITIZING WAND1",
+    description: "A rad description1",
+    button: "Shop Now!1",
+    backgroundColor: "f5fafd"
+  },
+  {
+    _id: 2,
+    image: "products/LED Sanitizing Wand 2.jpg",
+    title: "LED UVC SANITIZING WAND2",
+    description: "A rad description2",
+    button: "Shop Now!2",
+    backgroundColor: "f5fafd"
+  },
+  {
+    _id: 3,
+    image: "products/LED Sanitizing Wand 3.jpg",
+    title: "LED UVC SANITIZING WAND3",
+    description: "A rad description3",
+    button: "Shop Now!3",
+    backgroundColor: "f5fafd"
+  },
+  {
+    _id: 4,
+    image: "products/LED Sanitizing Wand 4.jpg",
+    title: "LED UVC SANITIZING WAND4",
+    description: "A rad description4",
+    button: "Shop Now!4",
+    backgroundColor: "f5fafd"
+  },
+  {
+    _id: 5,
+    image: "products/LED Sanitizing Wand 5.jpg",
+    title: "LED UVC SANITIZING WAND5",
+    description: "A rad description5",
+    button: "Shop Now!5",
+    backgroundColor: "f5fafd"
+  },
+  {
+    _id: 6,
+    image: "products/LED Sanitizing Wand 6.jpg",
+    title: "LED UVC SANITIZING WAND6",
+    description: "A rad description6",
+    button: "Shop Now!6",
+    backgroundColor: "f5fafd"
+  }
+];
